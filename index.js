7c17f4526258d20c7103d407a1ce67374b2eb216
Louis Clisson <l.clisson@hotmail.fr>
2020-08-21 11:50:56 +0200

    Refacto index.js in order to be more readable, maintainable and adaptable

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-
 var user = "BankinUser", password = "12345678", clientId = "BankinClientId", clientSecret= "secret";
 
 /**
@@ -19,18 +18,17 @@ async function getRefreshToken() {
       body: JSON.stringify({"user": user,"password": password}),
       redirect: 'follow'
     })
-  let data = await response.json()
-  return data
+  let data = await response.json();
+  let token = await data.refresh_token;
+  return token
 }
 
 /**
  * @description Trying to get the access token
  * @return the acces token
  */
-async function getAccessToken() {
+async function getAccessToken(refreshToken) {
   try {
-    var refreshToken = await getRefreshToken().then(data => data.refresh_token);
-
     var urlencoded = new URLSearchParams();
     urlencoded.append("grant_type", "refresh_token");
     urlencoded.append("refresh_token", refreshToken);
@@ -45,25 +43,25 @@ async function getAccessToken() {
         redirect: 'follow'
       })
     let data = await response.json();
-    return data
+    let accessToken = await data.access_token;
+    return accessToken
   } catch(error) {
     console.log(error);
   }
 }
 
-var dataAccounts = [];
 /**
- * @description Fetch accounts recursively
- * @param {Number} page Number of the page
+ * @description Fetch accounts until last page
  * @param {String} accessToken Access token to get the data needed
- * @return all of the accounts
+ * @return all of the unique accounts
  */
-async function getAccounts(page=1, accessToken=null) {
+async function getUniqueAccounts(accessToken) {
+  var dataAccounts = [];
   try {
-    if ( !accessToken){
-      var accessToken = await getAccessToken().then(data => getAccounts(page, data.access_token));  
-      return accessToken
-    } else {
+    var page = 1;
+    var hasNextPage = true;
+
+    while (hasNextPage) {
       console.log(`--- Fetch Accounts page n°${page} ---`)
       let response = await fetch(
         'http://localhost:3000/accounts?' + `page=${page}` ,
@@ -77,43 +75,34 @@ async function getAccounts(page=1, accessToken=null) {
           },
           redirect: 'follow'
         })
-      var data = await response.json();
-      let hasNextPage = data.link.next;
-      if (!hasNextPage) {
+        let data = await response.json();
+        hasNextPage = data.link.next;
         dataAccounts = dataAccounts.concat(data.account);
-        return dataAccounts
-      } else {
-        let nextPagesAccounts = await getAccounts(page + 1, (accessToken || null));
-        dataAccounts = data.account.concat(dataAccounts);
-        return dataAccounts
-      } 
-    }    
+        page +=1;
+    }
+    let uniqueAccount = dataAccounts.filter((v, i, a) => a.indexOfAccNumber(v.acc_number) === i);
+    return uniqueAccount
   } catch(error) {
     console.log(error);
   }
 }
 
-
-
 /**
- * @description Fetch all transactions from an account recursively
- * @param {String} id Account id 
- * @param {Number} page Number of the page
+ * @description Fetch all transactions from an account
+ * @param {String} accountNumber Account id 
  * @param {String} accessToken Number of the page
- * @return All of the transactions related to an account
+ * @return All unique transactions made by an account
  */
-async function getTransactions(id, page=1, accessToken){
+async function getUniqueTransactions(accountNumber, accessToken){
+  var dataTransactions = [];
   try {
-    if ( !accessToken){
-      var accessToken = await getAccessToken().then(data => getTransactions(id, page, data.access_token));  
-      return accessToken
-    } else if (!id){
-      return null      
-    } else {
-      var dataTransactions = [];
-      console.log(`--- Fetch Transaction page n°${page} of Account n°${id} ---`)
+    var page = 1;
+    var hasNextPage = true;
+
+    while(hasNextPage) {
+      console.log(`--- Fetch Transaction page n°${page} of Account n°${accountNumber} ---`)
       let response = await fetch(
-        'http://localhost:3000/accounts/' + id + '/transactions?' + `page=${page}` ,
+        'http://localhost:3000/accounts/' + accountNumber + '/transactions?' + `page=${page}` ,
         {
           method: 'GET',
           withCredentials: true,
@@ -125,86 +114,63 @@ async function getTransactions(id, page=1, accessToken){
           redirect: 'follow'
         })
       var data = await response.json();
-      let hasNextPage = data.link.next;
-      if (!hasNextPage) {
-        data.transactions = data.transactions.filter((v,i,a) => a.indexOfId(v.id)=== i).map(e => (e.sign === "DBT" ? changeSign(e,true) : changeSign(e, false)));
-        dataTransactions = dataTransactions.concat(data.transactions);
-        return dataTransactions
-      } else {
-        await getTransactions(id,page + 1, (accessToken || null)).then(res =>{
-          dataTransactions = dataTransactions.concat(data.transactions);
-          dataTransactions = dataTransactions.filter((v,i,a) => a.indexOfId(v.id)=== i).map(e => (e.sign === "DBT" ? changeSign(e,true) : changeSign(e, false)));
-        })
-        return dataTransactions
-      }
+      hasNextPage = data.link.next;
+      dataTransactions = dataTransactions.concat(data.transactions);
+      page +=1;
     }
+    dataTransactions = dataTransactions.filter((v,i,a) => a.indexOfId(v.id)=== i).map(transaction => parseTransaction(transaction));
+    return dataTransactions
   } catch(error) {
-    console.log(error.toString(), `--- Error with account id n°${id} at page n°${page}`)
+    console.log(error.toString(), `--- Error with account id n°${accountNumber} at page n°${page}`)
   }
 }
 
 /**
- * @description Change the transaction's amount depending of the sign of it (i.e CDT or DBT) and filter the data in the format needed
+ * @description Parse the transaction looking at the sign of the transaction
  * @param {Array} transaction transaction to process
- * @param {String} boolean boolean to tell us if we have to change it or not
- * @return a transaction process in the required format
+ * @return a transaction in the required format
  */
-function changeSign(transaction, boolean) {
-  if (!boolean) { let transactionInfo = {"label": transaction.label, "amount": transaction.amount, "currency": transaction.currency}; return transactionInfo}
-  else {let transactionInfo = {"label": transaction.label, "amount": "-" + transaction.amount, "currency": transaction.currency}; return transactionInfo}
+function parseTransaction(transaction) {
+  var transactionInfo = {"label": transaction.label, "amount": null, "currency": transaction.currency};
+  if (transaction.sign === "DBT" ) {
+    transactionInfo.amount = "-" + transaction.amount;
+  } else {
+    transactionInfo.amount = transaction.amount;
+  }
+  return transactionInfo
 }
 
-var transactions = [], accountsDone=[];
-/**
- * @description Parse all the transactions and accounts, and process the data.
- * @param {Array} accounts Accounts to process
- * @param {Int} i i is a pointer of the account we are processing in accounts -> accounts[i]
- * @param {String} accessToken A token required to access the data
- * @return All of the transactions parse in the format asked
- */
-async function parseTransactions(accounts=[],i=0 ,accessToken = null) {
+async function main() {
   try {
-    if ( !accessToken){
-      var accessToken = await getAccessToken().then(data => parseTransactions(accounts,i=0, data.access_token));  
+    const refreshToken = await getRefreshToken();
+    const accessToken = await getAccessToken(refreshToken);
+    var uniqueAccounts = await getUniqueAccounts(accessToken);
+
+    var result = [];
+    for (account of uniqueAccounts) {
+      transactions = await getUniqueTransactions(account.acc_number, accessToken);
+      accountTransactions = {"acc_number": account.acc_number, "amount": account.amount, "transactions": transactions};
+      result.push(accountTransactions);
+      console.log(accountTransactions);
     }
-    else if(accounts.length == 0) {
-      accounts = await getAccounts(page=1, accessToken).then((data) => {
-        var uniqueAccount = data.filter((v, i, a) => a.indexOfAccNumber(v.acc_number) === i); 
-        parseTransactions(accounts=uniqueAccount,i=0, accessToken)
-      })
-    } else {
-      if (i<accounts.length && (!accountsDone.includes(accounts[i]))) {
-        await getTransactions(accounts[i].acc_number,page=1, (accessToken || null)).then(res =>{
-                transactions.push({"acc_number": accounts[i].acc_number, "amount": accounts[i].amount, "transactions": res})
-                accountsDone.push(accounts[i])
-              }).then(res => parseTransactions(accounts, i+1, accessToken))  
-      } else if(i< accounts.length) {
-        parseTransactions(accounts, i+1, accessToken)
-      }else {
-        let disp = transactions.map(e => console.log(e))
-        return transactions
-      }
-    }
+    return result
   } catch(error) {
-    console.log(error);
+    console.log(error.toString())
   }
 }
 
-Array.prototype.indexOfAccNumber = function(id) {
+Array.prototype.indexOfAccNumber = function(id) { //Look at the index of an account number in an Array
   for (var i = 0; i < this.length; i++)
       if (this[i].acc_number === id)
           return i;
   return -1;
 }
 
-Array.prototype.indexOfId = function(id) {
+Array.prototype.indexOfId = function(id) { //Look at the index of an id in an Array
   for (var i = 0; i < this.length; i++)
       if (this[i].id === id)
           return i;
   return -1;
 }
 
-parseTransactions()
-
-// La fonction ci-dessous fonctionne et renvoie bien ce que l'on cherche, cependant je n'ai pas encore trouvé l'origine du problème. Le code reponse de la requête est 400. Cependant l'accesstoken est le bon et le compte "000000013" existe bel et bien.
-// getTransactions("000000013", page=1, null).then(res => console.log(res))
+main();
